d17a8831ef7234c4c31670733bcd2bfec6ddb99c
rafael barbosa <macbook@MacBook-Pro-2.local>
2021-10-01 22:46:17 -0300

    install terraform

--- 00-Setup-e-configuracao/installTerraform.sh
+++ 00-Setup-e-configuracao/installTerraform.sh
@@ -4,7 +4,7 @@ sudo apt-get install unzip -y
 #version=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')/terraform_$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')
 #echo $version
 #url="https://releases.hashicorp.com/terraform/$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')/terraform_$(curl -s https://checkpoint-api.hashicorp.com/v1/check/terraform | jq -r -M '.current_version')_linux_amd64.zip"
-url="https://releases.hashicorp.com/terraform/0.12.23/terraform_0.12.23_linux_amd64.zip"
+url="https://releases.hashicorp.com/terraform/terraform_1.0.8/terraform_1.0.8_linux_amd64.zip"
 echo $url
 wget $url
 
